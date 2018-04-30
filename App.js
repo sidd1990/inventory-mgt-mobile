df2b9312e6a915ff7be8c9b4d03b06017fe151c7
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-04-30 16:10:18 -0500

    Create App class component

--- App.js
+++ App.js
@@ -4,3 +4,14 @@ import {Provider} from 'react-native';
 import {createStore} from 'react-redux';
 import reducers from 'redux';
 import LoginForm from './src/components/LoginForm';
+
+class App extends Component {
+	render() {
+		return (
+			<Provider store={createStore(reducers)}>
+				<LoginForm/>
+			</Provider>
+		);
+	}
+}
+export default App;
