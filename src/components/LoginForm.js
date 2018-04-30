6f749f664c62d3e05d8425ca42f18f8aedf7677a
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-04-30 15:54:23 -0500

    Create login form component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Card, CardSection, Button, Input} from './common';
+
+class LoginForm extends Component {
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<Input label="Email" placeholder="email@gmail.com"/>
+				</CardSection>
+				<CardSection>
+					<Input label="Password" placeholder="password"/>
+				</CardSection>
+				<CardSection>
+					<Button>Login</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;
