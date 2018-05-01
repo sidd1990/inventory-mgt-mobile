73f38c4cd475127db35e2f93c085d26e3993125f
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 17:44:16 -0500

    Create boilerplate facility list

---
+++ src/components/FacilitiesList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class FacilitiesList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Facility List</Text>
+				<Text>Facility List</Text>
+				<Text>Facility List</Text>
+				<Text>Facility List</Text>
+				<Text>Facility List</Text>
+				<Text>Facility List</Text>
+			</View>
+		);
+	}
+}
+
+export default FacilitiesList;
