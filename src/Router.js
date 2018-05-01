6b1ea500349c446a27aa07dbf2e2ea72460b7c36
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 17:41:02 -0500

    Create base router

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Scene, Router} from 'react-native-router-flux';
+
+const RouterComponent = () => {
+	return (
+		<Router>
+			<Scene/>
+		</Router>
+	);
+};
+
+export default RouterComponent;
