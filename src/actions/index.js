7f353ae059c42f702e2cedc8496499619b0fc1dd
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 11:39:48 -0500

    Create basic login function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import {EMAIL_CHANGED, PASSWORD_CHANGED} from './types';
+import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS} from './types';
 
 export const emailChanged = text => {
 	return {
@@ -11,5 +11,15 @@ export const passwordChanged = text => {
 	return {
 		type: PASSWORD_CHANGED,
 		payload: text
+	};
+};
+
+export const loginUser = ({email, password}) => {
+	return dispatch => {
+		// TODO Fetch User From API
+		console.log('email and password: ' + email + ' ' + password);
+		// TODO Use dispatch in 'then' of axios.post
+		// TODO axios.post('url').then((user) => {FILL DISPATCH HERE})
+		// TODO dispatch(type: LOGIN_USER_SUCCESS, payload: user)
 	};
 };
