9fb117e943a1df4652eb7c7563a8005e82bf7f47
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 17:52:57 -0500

    Add app navigation using router

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './src/reducers';
-import LoginForm from './src/components/LoginForm';
+import Router from './src/Router';
 import ReduxThunk from 'redux-thunk';
 
 class App extends Component {
@@ -10,7 +10,7 @@ class App extends Component {
 		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 		return (
 			<Provider store={store}>
-				<LoginForm/>
+				<Router/>
 			</Provider>
 		);
 	}
