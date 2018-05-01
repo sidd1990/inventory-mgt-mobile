69848d992bf7c3f9a82f21711454ce3dead5697d
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 17:42:10 -0500

    Create two scenes for navigation

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Scene, Router} from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
 	return (
 		<Router>
-			<Scene/>
+			<Scene key="root">
+				<Scene key="login" component={LoginForm} title="Login" initial/>
+				<Scene key="facilities" component={FacilitiesList} title="Facilities"/>
+			</Scene>
 		</Router>
 	);
 };
