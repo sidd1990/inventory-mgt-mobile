dbf3a2fa8c27a5ad6f70b2612b8bc6454df1388a
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 12:00:49 -0500

    Add sample call using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS} from './types';
+import axios from 'axios';
 
 export const emailChanged = text => {
 	return {
@@ -21,5 +22,8 @@ export const loginUser = ({email, password}) => {
 		// TODO Use dispatch in 'then' of axios.post
 		// TODO axios.post('url').then((user) => {FILL DISPATCH HERE})
 		// TODO dispatch(type: LOGIN_USER_SUCCESS, payload: user)
+		axios.get('https://api.jikan.me/anime/1').then(data => {
+			dispatch({type: LOGIN_USER_SUCCESS, payload: data});
+		});
 	};
 };
