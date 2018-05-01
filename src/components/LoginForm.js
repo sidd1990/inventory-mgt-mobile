836b6bd62b46b12fb5a243a3328e8f89411790bb
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 11:41:55 -0500

    Add button press logic for login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Card, CardSection, Button, Input} from './common';
-import {emailChanged, passwordChanged} from '../actions';
+import {emailChanged, passwordChanged, loginUser} from '../actions';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
@@ -10,6 +10,11 @@ class LoginForm extends Component {
 
 	onPasswordChange(text) {
 		this.props.passwordChanged(text);
+	}
+
+	onButtonPress() {
+		const {email, password} = this.props;
+		this.props.loginUser({email, password});
 	}
 
 	render() {
@@ -33,7 +38,7 @@ class LoginForm extends Component {
 					/>
 				</CardSection>
 				<CardSection>
-					<Button>Login</Button>
+					<Button onPress={this.onButtonPress.bind(this)}>Login</Button>
 				</CardSection>
 			</Card>
 		);
@@ -48,6 +53,8 @@ const mapStateToProps = state => {
 	};
 };
 
-export default connect(mapStateToProps, {emailChanged, passwordChanged})(
-	LoginForm
-);
+export default connect(mapStateToProps, {
+	emailChanged,
+	passwordChanged,
+	loginUser
+})(LoginForm);
