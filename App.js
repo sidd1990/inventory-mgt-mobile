beef011571a5d30fa82184bfd41df05ba32f2b36
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-01 11:35:41 -0500

    Add and register redux thunk

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from './src/reducers';
 import LoginForm from './src/components/LoginForm';
+import ReduxThunk from 'redux-thunk';
 
 class App extends Component {
 	render() {
 		return (
-			<Provider store={createStore(reducers)}>
+			<Provider
+				store={(createStore(reducers), {}, applyMiddleware(ReduxThunk))}
+			>
 				<LoginForm/>
 			</Provider>
 		);
