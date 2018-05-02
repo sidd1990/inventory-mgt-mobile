8c4fe47f364a54fd96d87059e20e97ad2c716097
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-02 18:00:09 -0500

    Default reducer logic

---
+++ src/reducers/FacilitiesReducer.js
@@ -0,0 +1,9 @@
+import {FACILITY_FETCH_FAIL, FACILITY_FETCH_SUCCESS} from '../actions/types';
+
+const INITIAL_STATE = {};
+export default (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
