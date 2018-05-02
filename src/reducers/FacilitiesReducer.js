ebfcdef7638520961ba78d50899c37b505e9ca2c
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-02 18:02:37 -0500

    Handle facilities fetch fail

--- src/reducers/FacilitiesReducer.js
+++ src/reducers/FacilitiesReducer.js
@@ -1,10 +1,12 @@
 import {FACILITY_FETCH_FAIL, FACILITY_FETCH_SUCCESS} from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {facilities: null, error: ''};
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case FACILITY_FETCH_SUCCESS:
 			return {...state, facilities: action.payload};
+		case FACILITY_FETCH_FAIL:
+			return {...state, error: 'Unable to fetch facilities'};
 		default:
 			return state;
 	}
