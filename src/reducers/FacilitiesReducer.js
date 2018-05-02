c2988db84701f4bf66c16fce31d69acf165f5622
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-02 18:01:36 -0500

    Handle successful fetch

--- src/reducers/FacilitiesReducer.js
+++ src/reducers/FacilitiesReducer.js
@@ -3,6 +3,8 @@ import {FACILITY_FETCH_FAIL, FACILITY_FETCH_SUCCESS} from '../actions/types';
 const INITIAL_STATE = {};
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case FACILITY_FETCH_SUCCESS:
+			return {...state, facilities: action.payload};
 		default:
 			return state;
 	}
