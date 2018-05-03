ec7bed061b4551ac8e0bf8e8600a72465602d28e
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 13:24:08 -0500

    Create facilities list item

---
+++ src/components/FacilityListItem.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+import {CardSection} from './common';
+
+const styles = {
+	titleStyle: {
+		paddingLeft: 15,
+		fontSize: 18
+	}
+};
+
+class FacilityListItem extends Component {
+	render() {
+		const {name} = this.props.facilities;
+		return (
+			<CardSection>
+				<Text style={styles.titleStyle}>{name}</Text>
+			</CardSection>
+		);
+	}
+}
+
+export default FacilityListItem;
