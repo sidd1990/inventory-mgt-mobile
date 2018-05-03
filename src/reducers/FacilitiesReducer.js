b535c60cc37179879a8eac698e9540ed02592d33
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 14:00:53 -0500

    Change type to empty array

--- src/reducers/FacilitiesReducer.js
+++ src/reducers/FacilitiesReducer.js
@@ -1,6 +1,6 @@
 import {FACILITY_FETCH_FAIL, FACILITY_FETCH_SUCCESS} from '../actions/types';
 
-const INITIAL_STATE = {facilities: null, error: ''};
+const INITIAL_STATE = {facilities: [], error: ''};
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case FACILITY_FETCH_SUCCESS:
