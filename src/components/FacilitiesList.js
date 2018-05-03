361237b4e7045fe862b03891904456e7dbe67b99
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 18:47:46 -0500

    Add comments

--- src/components/FacilitiesList.js
+++ src/components/FacilitiesList.js
@@ -3,17 +3,30 @@ import {connect} from 'react-redux';
 import {ListView} from 'react-native';
 import {facilitiesFetch} from '../actions';
 import FacilityListItem from './FacilityListItem';
-
+/**
+ * Class component for rendering a list of facilities attached to a resource checker
+ */
 class FacilitiesList extends Component {
+	/**
+	 * Called when component is first created
+	 */
 	componentWillMount() {
 		this.props.facilitiesFetch({uid: this.props.user.userid});
 		this.createDataSource(this.props);
 	}
 
+	/**
+	 * Called when component is about to receive new props
+	 * @param {Object} newProps New props object
+	 */
 	componentWillReceiveProps(newProps) {
 		this.createDataSource(newProps);
 	}
 
+	/**
+	 * Creates a data source for the list view
+	 * @param {Object} param0 Object with all facilities in array
+	 */
 	createDataSource({facilities}) {
 		const ds = new ListView.DataSource({
 			rowHasChanged: (r1, r2) => r1 !== r2
@@ -21,10 +34,19 @@ class FacilitiesList extends Component {
 		this.dataSource = ds.cloneWithRows(facilities);
 	}
 
+	/**
+	 * Renders a row for the list view
+	 * @param {Object} facility A single facility object
+	 * @returns {Component} New facility
+	 */
 	renderRow(facility) {
 		return <FacilityListItem facility={facility}/>;
 	}
 
+	/**
+	 * Renders the list of facilities
+	 * @returns {Component} List view
+	 */
 	render() {
 		return (
 			<ListView
@@ -35,7 +57,11 @@ class FacilitiesList extends Component {
 		);
 	}
 }
-
+/**
+ * Maps state to props
+ * @param {Object} state State object from redux
+ * @returns {Object} User and facilities props
+ */
 const mapStateToProps = state => {
 	const {user} = state.auth;
 	const {facilities} = state.facilities;
