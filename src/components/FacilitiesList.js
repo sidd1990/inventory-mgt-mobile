6c03b6dc2e2f11e79225a21f4b59569fd5333d43
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 14:00:26 -0500

    Remove Text import

--- src/components/FacilitiesList.js
+++ src/components/FacilitiesList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {ListView, Text} from 'react-native';
+import {ListView} from 'react-native';
 import {facilitiesFetch} from '../actions';
 import FacilityListItem from './FacilityListItem';
 
