fe26662e8fc0e1d8beea6222cd3a53db33af8980
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 16:00:11 -0500

    Import Actions object

--- src/components/ResourceListItem.js
+++ src/components/ResourceListItem.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View, TouchableWithoutFeedback, Text} from 'react-native';
+import {Actions} from 'react-native-router-flux';
 import {CardSection} from './common';
 
 const styles = {
