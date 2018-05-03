d203ec13bb7c8850e149c23228e9ab31e805763f
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 15:36:43 -0500

    Return a button with text

--- src/components/ResourceListItem.js
+++ src/components/ResourceListItem.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import {View, TouchableWithoutFeedback, Text} from 'react-native';
 import {CardSection} from './common';
 
-class ResourceListItem extends Component {}
+class ResourceListItem extends Component {
+	render() {
+		const {Name} = this.props.resource;
+		return (
+			<TouchableWithoutFeedback>
+				<View>
+					<CardSection>
+						<Text style={styles.titleStyle}>{Name}</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
+		);
+	}
+}
 
 export default ResourceListItem;
