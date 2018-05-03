7356bea7ec4bc073bc3b1cec27893f2bbd10dcb9
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 15:15:32 -0500

    Add resource fetch logic

---
+++ src/actions/ResourcesActions.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {RESOURCE_FETCH_FAIL, RESOURCE_FETCH_SUCCESS} from './types';
+
+const fetchResourceSuccess = (dispatch, data) => {
+	dispatch({
+		type: RESOURCE_FETCH_SUCCESS,
+		payload: data
+	});
+};
+
+export const fetchResources = ({facilityId}) => {
+	return dispatch => {
+		axios
+			.get(
+				`https://pu65y7gbt8.execute-api.us-east-1.amazonaws.com/dev/get/resourceverify/facilityid/${facilityId}`
+			)
+			.then(data => {
+				fetchResourceSuccess(dispatch, data);
+			})
+			.catch(error => {
+				console.log('Error fetching resource: ' + error);
+				dispatch({
+					type: RESOURCE_FETCH_FAIL
+				});
+			});
+	};
+};
