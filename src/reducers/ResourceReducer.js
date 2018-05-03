c4b40e6a614f05e0a75f021aebe95442dd6dee33
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 16:23:30 -0500

    Add reducer case for quantity changed

--- src/reducers/ResourceReducer.js
+++ src/reducers/ResourceReducer.js
@@ -1,6 +1,10 @@
-import {RESOURCE_FETCH_FAIL, RESOURCE_FETCH_SUCCESS} from '../actions/types';
+import {
+	RESOURCE_FETCH_FAIL,
+	RESOURCE_FETCH_SUCCESS,
+	QUANTITY_CHANGED
+} from '../actions/types';
 
-const INITIAL_STATE = {resources: [], error: ''};
+const INITIAL_STATE = {resources: [], error: '', quantity: 0};
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
@@ -8,6 +12,8 @@ export default (state = INITIAL_STATE, action) => {
 			return {...INITIAL_STATE, resources: action.payload};
 		case RESOURCE_FETCH_FAIL:
 			return {...INITIAL_STATE, error: 'Error while retrieving data'};
+		case QUANTITY_CHANGED:
+			return {...state, quantity: action.payload};
 		default:
 			return state;
 	}
