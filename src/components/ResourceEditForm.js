7b59b348f1aaf1d72ee9c590cf31030da64a4826
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 17:49:58 -0500

    Disable buttons if resource verified

--- src/components/ResourceEditForm.js
+++ src/components/ResourceEditForm.js
@@ -24,6 +24,45 @@ class ResourceEditForm extends Component {
 		this.props.markAsMissing({resourceId: this.props.resource.Id});
 	}
 
+	renderConfirm() {
+		if (this.props.resource.Verified) {
+			return (
+				<Button disabled onPress={this.onConfirmClick.bind(this)}>
+					Confirm Quantity
+				</Button>
+			);
+		}
+		return (
+			<Button onPress={this.onConfirmClick.bind(this)}>Confirm Quantity</Button>
+		);
+	}
+
+	renderUpdate() {
+		if (this.props.resource.Verified) {
+			return (
+				<Button disabled onPress={this.onUpdateClick.bind(this)}>
+					Update Quantity
+				</Button>
+			);
+		}
+		return (
+			<Button onPress={this.onUpdateClick.bind(this)}>Update Quantity</Button>
+		);
+	}
+
+	renderMarkMissing() {
+		if (this.props.resource.Verified) {
+			return (
+				<Button disabled onPress={this.onMarkClick.bind(this)}>
+					Mark as Missing
+				</Button>
+			);
+		}
+		return (
+			<Button onPress={this.onMarkClick.bind(this)}>Mark as Missing</Button>
+		);
+	}
+
 	render() {
 		return (
 			<Card>
@@ -38,19 +77,9 @@ class ResourceEditForm extends Component {
 						}}
 					/>
 				</CardSection>
-				<CardSection>
-					<Button onPress={this.onConfirmClick.bind(this)}>
-						Confirm Quantity
-					</Button>
-				</CardSection>
-				<CardSection>
-					<Button onPress={this.onUpdateClick.bind(this)}>
-						Update Quantity
-					</Button>
-				</CardSection>
-				<CardSection>
-					<Button onPress={this.onMarkClick.bind(this)}>Mark as Missing</Button>
-				</CardSection>
+				<CardSection>{this.renderConfirm()}</CardSection>
+				<CardSection>{this.renderUpdate()}</CardSection>
+				<CardSection>{this.renderMarkMissing()}</CardSection>
 			</Card>
 		);
 	}
