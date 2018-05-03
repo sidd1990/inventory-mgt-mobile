f50cf9d0390a9e598209c2ae69b746e441e566e4
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 16:07:18 -0500

    Create resource edit form

--- src/components/ResourceEditForm.js
+++ src/components/ResourceEditForm.js
@@ -2,3 +2,32 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Text} from 'react-native';
 import {Button, Card, CardSection, Input} from './common';
+
+class ResourceEditForm extends Component {
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<Text>{this.props.resource.Name}</Text>
+				</CardSection>
+				<CardSection>
+					<Input
+						label="Quantity"
+						placeholder="10"
+						value={this.props.resource.Quantity}
+						onChangeText={() => {}}
+					/>
+				</CardSection>
+				<CardSection>
+					<Button>Confirm Quantity</Button>
+				</CardSection>
+				<CardSection>
+					<Button>Update Quantity</Button>
+				</CardSection>
+				<CardSection>
+					<Button>Mark as Missing</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
