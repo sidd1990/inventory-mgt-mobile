62c3675ca6a4e55f8c3b200fa224f73e8af0cbc2
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 15:35:11 -0500

    Create new component

--- src/components/ResourceListItem.js
+++ src/components/ResourceListItem.js
@@ -1,3 +1,7 @@
 import React, {Component} from 'react';
 import {View, TouchableWithoutFeedback, Text} from 'react-native';
 import {CardSection} from './common';
+
+class ResourceListItem extends Component {}
+
+export default ResourceListItem;
