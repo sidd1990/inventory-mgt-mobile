5a1821e9307bdfdb52000f2a5e2d9fefa1330669
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-03 18:40:17 -0500

    Add comments

--- src/actions/FacilitiesActions.js
+++ src/actions/FacilitiesActions.js
@@ -1,19 +1,32 @@
 import axios from 'axios';
 import {FACILITY_FETCH_FAIL, FACILITY_FETCH_SUCCESS} from '../actions/types';
 
+/**
+ * * Creates new action of specific type
+ * @param {Function} dispatch Used to send action asynchronously
+ * @param {Array} data Array object containing all data
+ */
 const fetchDataSuccess = (dispatch, data) => {
 	dispatch({
 		type: FACILITY_FETCH_SUCCESS,
 		payload: data
 	});
 };
-
+/**
+ * * Creates new action of specific type
+ * @param {Function} dispatch Used to send action in async
+ */
 const fetchDataFail = dispatch => {
 	dispatch({
 		type: FACILITY_FETCH_FAIL
 	});
 };
 
+/**
+ * * Attempts to fetch facilities for user
+ * @param {Object} uid User id to fetch facilities for
+ * @returns {Function} New action using dispatch
+ */
 export const facilitiesFetch = ({uid}) => {
 	return dispatch => {
 		axios
