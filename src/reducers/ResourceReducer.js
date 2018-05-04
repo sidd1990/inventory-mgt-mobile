7945680827d83be7209a061958f556003a2b1c62
Vibhor Kainth <kainthvibhor7@gmail.com>
2018-05-04 16:27:04 -0500

    Create reducers

--- src/reducers/ResourceReducer.js
+++ src/reducers/ResourceReducer.js
@@ -1,7 +1,10 @@
 import {
 	RESOURCE_FETCH_FAIL,
 	RESOURCE_FETCH_SUCCESS,
-	QUANTITY_CHANGED
+	QUANTITY_CHANGED,
+	CONFIRM_RESOURCE_QUANTITY,
+	UPDATE_RESOURCE_QUANTITY,
+	MARK_RESOURCE_MISSING
 } from '../actions/types';
 
 const INITIAL_STATE = {resources: [], error: '', quantity: 0};
@@ -14,6 +17,12 @@ export default (state = INITIAL_STATE, action) => {
 			return {...INITIAL_STATE, error: 'Error while retrieving data'};
 		case QUANTITY_CHANGED:
 			return {...state, quantity: action.payload};
+		case CONFIRM_RESOURCE_QUANTITY:
+			return state;
+		case UPDATE_RESOURCE_QUANTITY:
+			return state;
+		case MARK_RESOURCE_MISSING:
+			return state;
 		default:
 			return state;
 	}
